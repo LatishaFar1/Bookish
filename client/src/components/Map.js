48efd234358b7e03649411fd0f6d3691b94e1329
Tish Faroul <lfaroul14@gmail.com>
2022-01-29 20:01:18 -0500

    continuing map

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,16 +1,40 @@
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import React, {useState} from "react";
+
 export default function Map() {
 
-
+    const containerStyle = {
+        width: '400px',
+        height: '400px'
+      };
+      
+      const center = {
+        lat: -3.745,
+        lng: -38.523
+      };
+      
+      function MyComponent() {
+        const { isLoaded } = useJsApiLoader({
+          id: 'google-map-script',
+          googleMapsApiKey: "AIzaSyCaPsMiWKYIXhHeVUJi02-lEYbLSSpG7Lg"
+        }) }
+      
+        const [map, setMap] = React.useState(null)
+      
+        const onLoad = React.useCallback(function callback(map) {
+          const bounds = new window.google.maps.LatLngBounds();
+          map.fitBounds(bounds);
+          setMap(map)
+        }, [])
+      
+        const onUnmount = React.useCallback(function callback(map) {
+          setMap(null)
+        }, [])
 
 
   return <div id="map">
       <h1>Map</h1>
-
-    <script
-      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCaPsMiWKYIXhHeVUJi02-lEYbLSSpG7Lg&callback=initMap&libraries=&v=weekly"
-      async
-    ></script>
-
+    
 
  {/* aPI key = AIzaSyCaPsMiWKYIXhHeVUJi02-lEYbLSSpG7Lg */}
 
