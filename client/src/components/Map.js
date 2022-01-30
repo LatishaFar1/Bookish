f96d21519bccf5b4e242d0189c1b64f7be3994f1
Tish Faroul <lfaroul14@gmail.com>
2022-01-29 19:43:40 -0500

    started map

---
+++ client/src/components/Map.js
@@ -0,0 +1,18 @@
+export default function Map() {
+
+
+
+
+  return <div id="map">
+      <h1>Map</h1>
+
+    <script
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCaPsMiWKYIXhHeVUJi02-lEYbLSSpG7Lg&callback=initMap&libraries=&v=weekly"
+      async
+    ></script>
+
+
+ {/* aPI key = AIzaSyCaPsMiWKYIXhHeVUJi02-lEYbLSSpG7Lg */}
+
+  </div>;
+}
