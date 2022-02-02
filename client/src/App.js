8a6e6e80f4ecaf7b947f83a24c88150aeb10dd0a
Tish Faroul <lfaroul14@gmail.com>
2022-02-01 20:05:27 -0500

    changing map

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,15 @@
 import Map from './components/Map';
-import Marker from './components/Marker';
-import { Wrapper } from "@googlemaps/react-wrapper";
+
 import './App.css';
 
 function App() {
-  const center = {lat: 40.713050, lng: -74.007230}
+  
 
 
   return (
-    <Wrapper apiKey={""}>
-      <Map>
-        <Marker position = {center}/>
-      </Map>
-
-    </Wrapper>
+   <div>
+     <h1>Hi</h1>
+   </div>
     
    
   );
--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,18 +1,11 @@
 
-import React, {useEffect, useRef} from "react";
+import React from "react";
 
-export default function Map({children}) {
-    const ref = useRef();
-
-    useEffect(() => {
-      new window.google.maps.Map(ref.current, {
-        center: {lat: 40.713050, lng: -74.007230 },
-        zoom: 12,
-      });
-    });
+export default function Map() {
    
-  return <div ref={ref} id="map">
-     {children}
+   
+  return <div>
+     
     
 
 
