0f3c8b0053a5e90177b305ce8f525cbdb611b442
Tish Faroul <lfaroul14@gmail.com>
2022-02-01 20:37:39 -0500

    new map basic functions working

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,10 +1,18 @@
 
 import React from "react";
 
+
 export default function Map() {
+  const google = window.google 
+
+  
+  const map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 40.712776, lng: -74.005974},
+    zoom: 8,
+    mapId: '2ef2dbafe148d19'
+    });
    
-   
-  return <div>
+  return <div className="map">
      
     
 
