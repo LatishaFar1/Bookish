6b30816672a10593409e895d67943698b3e59341
Tish Faroul <lfaroul14@gmail.com>
2022-02-01 20:48:58 -0500

    added a marker

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -8,10 +8,16 @@ export default function Map() {
   
   const map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 40.712776, lng: -74.005974},
-    zoom: 8,
+    zoom: 14.5,
     mapId: '2ef2dbafe148d19'
     });
    
+    new google.maps.Marker({
+      position: {lat: 40.712511, lng: -74.007905},
+      map,
+      title: "Hello World!",
+    });
+
   return <div className="map">
      
     
