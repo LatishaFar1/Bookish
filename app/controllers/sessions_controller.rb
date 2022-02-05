64780ab830848e3f7c182473b52fd233e710e019
Tish Faroul <lfaroul14@gmail.com>
2022-02-04 23:57:32 -0500

    worked on sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,17 @@
 class SessionsController < ApplicationController
+    def create
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, status: :ok
+        else
+            render json: {error: "User cannot be found"}, status: :unauthorized
+        end 
+    end 
+
+
+    def destroy
+        session.destroy
+        render json: {"loggedOut": true}
+    end 
 end
