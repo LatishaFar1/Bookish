4f91ffc5c6dab40b09c7534c102fe38edf643573
Tish Faroul <lfaroul14@gmail.com>
2022-02-04 23:48:47 -0500

    started books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,22 @@
 class BooksController < ApplicationController
+
+    def index 
+        books = Book.all
+        render json: books
+    end 
+
+    def show 
+        book = Book.find(params[:id])
+        render json: book
+    end 
+
+    def create
+        book = Book.create(book_params)
+        render json: book 
+    end 
+
+
+    def book_params
+        params.permit(:title, :author, :stock, :image)
+    end 
 end
