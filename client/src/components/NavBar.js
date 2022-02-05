fdfab17a10e08e7677213a7c38dd1f3369c7258d
Tish Faroul <lfaroul14@gmail.com>
2022-02-04 23:43:29 -0500

    seeded some data

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -5,13 +5,11 @@ export default function NavBar() {
 
   return <div>
       <div className='nav-container'>
-        
-
-
+        <ul className='nav-links'>
+            <li className="nav-link">
+                
+            </li>
+        </ul>
       </div>
-
-
-
-
   </div>;
 }
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+    book1 = Book.create(
+        title: "Edgar Allan Poe Collected Works",
+        author: "Edgar Allan Poe",
+        stock: 2,
+        image: "https://images-na.ssl-images-amazon.com/images/I/71lUZ4yLB8S.jpg"
+    )
+
+    book2 = Book.create(
+        title: "His Dark Materials",
+        author: "Philip Pullman",
+        stock: 1,
+        image: "https://images-na.ssl-images-amazon.com/images/I/91zAe4EXmjL.jpg"
+    )
+
+    puts "seeded."
