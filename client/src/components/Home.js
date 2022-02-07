69f9ad87e3de75176ac4b9faf504024513fba649
Tish Faroul <lfaroul14@gmail.com>
2022-02-06 20:32:19 -0500

    changed around the home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,29 +4,21 @@ export default function Home() {
 
 
 
-  return <div>
+  return <div style={{backgroundColor: "#060e10"}}>
 
 <section className="hero is-large is-info">
   <div className="hero-body">
     <p className="title">
      <img src="https://images.unsplash.com/photo-1535905557558-afc4877a26fc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80" 
      alt="pic of books"
-     style={{height: "50%", width: "50%", marginLeft: "auto", marginRight: "auto", display: "block"}}/>
+     style={{height: "50%", width: "50%", marginLeft: "auto", display: "block"}}/>
     </p>
     <p className="subtitle">
-      Large subtitle
+      text about the site 
     </p>
   </div>
 </section>
                 
-<section className="section" style={{backgroundColor: "#B23A48"}}>
-  <h1 className="title">Available Books:</h1>
-  <h2 className="subtitle">
-    ...
-  </h2>
-</section>
-
-
 
   </div>;
 }
