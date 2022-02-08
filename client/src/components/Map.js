9efb082b1675ecece2fdaefe46728fad51fece6d
Tish Faroul <lfaroul14@gmail.com>
2022-02-07 23:34:26 -0500

    map is done

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -9,26 +9,37 @@ export default function Map() {
 
   
   const map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.712776, lng: -74.005974},
-    zoom: 16,
+    center: {lat: 40.728378, lng: -73.993876},
+    zoom: 13.5,
     mapId: '2ef2dbafe148d19'
     });
    
 
+
+    const locations = [
+      ["Bookish HQ", 40.728378, -73.993876, 1],
+      ["Bookish Location", 40.742816, -73.999112, 2],
+      ["Bookish Soho Location", 40.724605, -74.002987, 3]
+    ]
+
+    for (let i=0; i < locations.length; i++){
+      const location = locations[i];
+
     const marker = new google.maps.Marker({
-      position: {lat: 40.712511, lng: -74.007905},
+      position: {lat: location[1], lng: location[2]},
       map,
-      title:"bookish location",
+      title: location[0],
       animation: google.maps.Animation.DROP
     });
 
     const infoWindow = new google.maps.InfoWindow({
-      content: "Bookish Location"
+      content: location[0]
     })
 
     marker.addListener("click", () => {
       infoWindow.open(map, marker)
     });
+    }
 
   return <div className="map">
      
