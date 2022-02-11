39a7a96802e9ee117a4f406ae4fbc4f87442e7e0
Tish Faroul <lfaroul14@gmail.com>
2022-02-10 21:37:31 -0500

    beginning cart

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -9,13 +9,18 @@ export default function Home() {
 <section>
       <h1 className='home-title'>BOOKISH</h1>
   <div className="home-body">
-    <p className="title">
     <p className="subtitle">
+      <span>
       Welcome to Bookish 
-    </p>
+      <br/>
+      Here, you can buy used books from one of our many locations around NYC. 
+      <br/>
+      Check out the map below to find us!
+
      <img src="https://images.unsplash.com/photo-1535905557558-afc4877a26fc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80" 
      alt="pic of books"
      style={{height: "50%", width: "50%", marginLeft: "auto", display: "block"}}/>
+   </span>
     </p>
   </div>
 </section>
