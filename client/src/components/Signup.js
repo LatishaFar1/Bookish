3fc66c1b0776480f35a64f4147d1b0a46210f9e4
Tish Faroul <lfaroul14@gmail.com>
2022-02-13 15:19:15 -0500

    login and signup work

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -30,7 +30,32 @@ export default function Signup({setUser}) {
 
 
 
-  return (
-    <div>Signup</div>
-  )
+  return <div style={{backgroundColor: "#1E1F24"}}>
+
+<div className="login-container">
+                  <div className="login-screen">
+                    <form className="login-form" onSubmit={handleSubmit}>
+                      <div className="login-info">
+                        <input type="text" className="login-input" 
+                        value={username} onChange={(e) => setUsername(e.target.value)} />
+                        <label>username</label>
+                      </div>
+                      <div className="login-info">
+                        <input type="password" className="login-input"
+                        value={password} onChange={(e) => setPassword(e.target.value)}/>
+                        <label>password</label>
+                      </div>
+                      <button className='login-button' type="submit"> signup </button>
+                    </form>
+                  </div>
+
+
+                </div>
+        
+
+
+
+
+
+  </div>
 }
